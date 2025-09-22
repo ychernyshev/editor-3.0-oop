b2260eaa6ca02226ac96b929de6f1168396738e9
Eugene <eugene_chernyshev@yahoo.com>
2025-09-22 17:47:27 +0300

    MenuBarPanelButton.js fixes
    - MenuBarPanelButton class rewritten

--- components/buttons/MenuBarPanelButton.js
+++ components/buttons/MenuBarPanelButton.js
@@ -1,24 +1,17 @@
 import { AbstractButton } from "../abstract/AbstractButton.js";
 
 export class MenuBarPanelButton extends AbstractButton {
-    constructor(containerID, labels, ButtonClass) {
+    constructor(label) {
         super();
-        this.container = document.getElementById(containerID);
-        this.labels = labels;
-        this.ButtonClass = ButtonClass;
-        this.buttons = [];
+        this.label = label;
+        this.container = document.getElementById("menuBarContainer");
     }
 
     render() {
-        this.labels.forEach(label => {
-            const button = new this.ButtonClass(label, this.container);
-            button.render();
-            this.buttons.push(button);
-        })
-        // const button = document.createElement("button");
-        // button.innerText = this.label;
-        // button.className = "bg-light p-1 border-0";
-        // this.setButton(button);
-        // this.menuBarContainer.appendChild(button);
+        const button = document.createElement("button");
+        button.innerText = this.label;
+        button.className = "bg-light p-1 border-0";
+        this.setButton(button);
+        this.container.appendChild(button);
     }
 }
--- core/main.js
+++ core/main.js
@@ -4,18 +4,14 @@ import { MenuBarPanelButton } from '../components/buttons/MenuBarPanelButton.js'
 
 const menuBar = new MenuBar();
 const textArea = new TextArea();
-// const MenuButton = new MenuBarPanelButton;
 
 menuBar.renderMenuBarContainer();
 textArea.renderTextAreaContainer();
 
 // Menu Bar Panels name
-// const buttonLabels = ["home", "insert"];
-//
-// buttonLabels.forEach(label => {
-//     const button = new MenuBarPanelButton(label);
-//     button.render();
-// })
+const buttonLabels = ["home", "insert"];
 
-const buttonManager = new MenuBarPanelButton("menuBarContainer", ["home", "insert"], MenuBarPanelButton)
-buttonManager.render();
+buttonLabels.forEach(label => {
+    const button = new MenuBarPanelButton(label);
+    button.render();
+})
