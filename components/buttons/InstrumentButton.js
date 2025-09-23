d6301fd1524d4e9798f3d17be96c6a832844f389
Eugene <eugene_chernyshev@yahoo.com>
2025-09-23 19:30:35 +0300

    Create InstrumentButton.js
    Created the InstrumentButton.js blank script

---
+++ components/buttons/InstrumentButton.js
@@ -0,0 +1,11 @@
+import {AbstractButton} from "../abstract/AbstractButton.js";
+
+export class InstrumentButton extends AbstractButton {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+    }
+}
