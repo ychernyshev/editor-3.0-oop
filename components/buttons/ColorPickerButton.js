fbce9b3adee00e3e481cebdd31b8d473487fe982
Eugene <eugene_chernyshev@yahoo.com>
2025-09-26 13:59:22 +0300

    Add color picker button
    UI: Added the color picker selection menu

---
+++ components/buttons/ColorPickerButton.js
@@ -0,0 +1,27 @@
+import {AbstractButton} from "../abstract/AbstractButton.js";
+
+export class ColorPickerButton extends AbstractButton {
+    constructor(colour) {
+        super();
+        this.colour = colour;
+    }
+
+    render() {
+        const container = document.getElementById("home-tab-pane");
+        const select = document.createElement("select");
+
+        select.className = "form-control text-secondary";
+
+        this.colour.forEach(color => {
+            const option = document.createElement("option");
+            option.style.backgroundColor = color;
+            option.value = color;
+            option.textContent = color;
+            select.appendChild(option);
+        })
+
+        this.setButton(select);
+
+        container.appendChild(select);
+    }
+}
--- core/main.js
+++ core/main.js
@@ -1,9 +1,10 @@
-import  { MenuBar } from '../components/MenuBar.js';
-import { TextArea } from '../components/TextArea.js';
-import { MenuBarSelectPanelButton } from '../components/buttons/MenuBarSelectPanelButton.js';
+import {MenuBar} from '../components/MenuBar.js';
+import {TextArea} from '../components/TextArea.js';
+import {MenuBarSelectPanelButton} from '../components/buttons/MenuBarSelectPanelButton.js';
 import {InstrumentsTab} from "../components/menu-bar/InstrumentsTab.js";
 import {InstrumentSelect} from "../components/menu-bar/InstrumentSelect.js";
 import {InstrumentButton} from "../components/buttons/InstrumentButton.js";
+import {ColorPickerButton} from "../components/buttons/ColorPickerButton.js";
 
 const menuBar = new MenuBar();
 const textArea = new TextArea();
@@ -32,7 +33,11 @@ tabPanels.forEach(tabID => {
 
 // Menu Bar Input
 const selectList = [
-    {"name": "fonts", "alt": "Font Family", "value": ["Arial", "Geirgia", "Impact", "Tahoma", "Times New Roman", "Vardana"]},
+    {
+        "name": "fonts",
+        "alt": "Font Family",
+        "value": ["Arial", "Geirgia", "Impact", "Tahoma", "Times New Roman", "Vardana"]
+    },
     {"name": "numbers", "alt": "Font Size", "value": [8, 10, 12, 14, 16, 18, 20, 22, 25, 30]},
 ]
 
@@ -47,23 +52,60 @@ const fontWeight = [
     {"styleType": "fontStyle", "style": "italic", "sample": "B", "code": ""},
     {"styleType": "textDecoration", "style": "underline", "sample": "C", "code": ""},
     {"styleType": "textDecoration", "style": "line-through", "sample": "Abc", "code": ""},
-    {"styleType": "text-align", "style": "left", "sample": "", "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-text-left\" viewBox=\"0 0 16 16\">\n" +
+    {
+        "styleType": "text-align",
+        "style": "left",
+        "sample": "",
+        "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-text-left\" viewBox=\"0 0 16 16\">\n" +
             "  <path fill-rule=\"evenodd\" d=\"M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5\"/>\n" +
-            "</svg>"},
-    {"styleType": "text-align", "style": "center", "sample": "", "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-text-right\" viewBox=\"0 0 16 16\">\n" +
+            "</svg>"
+    },
+    {
+        "styleType": "text-align",
+        "style": "center",
+        "sample": "",
+        "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-text-right\" viewBox=\"0 0 16 16\">\n" +
             "  <path fill-rule=\"evenodd\" d=\"M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5\"/>\n" +
-            "</svg>"},
-    {"styleType": "text-align", "style": "right", "sample": "", "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-text-center\" viewBox=\"0 0 16 16\">\n" +
+            "</svg>"
+    },
+    {
+        "styleType": "text-align",
+        "style": "right",
+        "sample": "",
+        "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-text-center\" viewBox=\"0 0 16 16\">\n" +
             "  <path fill-rule=\"evenodd\" d=\"M4 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5\"/>\n" +
-            "</svg>"},
-    {"styleType": "text-align", "style": "justify", "sample": "", "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-justify\" viewBox=\"0 0 16 16\">\n" +
+            "</svg>"
+    },
+    {
+        "styleType": "text-align",
+        "style": "justify",
+        "sample": "",
+        "code": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-justify\" viewBox=\"0 0 16 16\">\n" +
             "  <path fill-rule=\"evenodd\" d=\"M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5\"/>\n" +
-            "</svg>"},
+            "</svg>"
+    },
 ]
 
 fontWeight.forEach(style => {
     const fontWrightButton = new InstrumentButton(style.styleType, style.style, style.sample, style.code);
     fontWrightButton.render();
+})
+
+// Color Picker
+const colorPicker = [
+    {
+        "colour": ["#ef5350", "#b71c1c", "#d50000", "#560027",
+            "#9c27b0", "#6a1b9a", "#38006b", "#6200ea",
+            "#5c6bc0", "#1a237e", "#000051", "#2962ff",
+            "#6ff9ff", "#00838f", "#f9a825", "#f57f17",
+            "#ffa726", "#43a047", "#00c853", "#cddc39",
+            "#ffeb3b"]
+    }
+]
+
+colorPicker.forEach(color => {
+    const picker = new ColorPickerButton(color.colour);
+    picker.render();
 })
 
 // Text Area Panel
